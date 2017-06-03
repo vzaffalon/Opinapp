ac131979eb49c0f859570789a2712b2f99fdab6e
vzaffalon <zaffalonvictor@gmail.com>
2017-06-03 16:38:29 -0300

    perfil search layout

---
+++ app/src/main/java/com/opinnapp/opinnapp/tabholder/explore/tabs/PerfilAdapter.java
@@ -0,0 +1,89 @@
+package com.opinnapp.opinnapp.tabholder.explore.tabs;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.opinnapp.opinnapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by vzaffalon on 19/05/17.
+ */
+
+public class PerfilAdapter extends RecyclerView.Adapter<PerfilAdapter.ViewHolder> {
+    private List<Perfil> perfils;
+    private Context context;
+    private final OnItemClickListener clickListener;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView perfilName;
+        public CircleImageView perfilPicture;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            perfilName = (TextView) itemView.findViewById(R.id.cell_perfil_name);
+            perfilPicture = (CircleImageView) itemView.findViewById(R.id.cell_perfil_picture);
+        }
+
+        public void bind(final Perfil item, final OnItemClickListener listener) {
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(item);
+                }
+            });
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public PerfilAdapter(List<Perfil> mPerfils, Context mContext, OnItemClickListener listener) {
+        perfils = mPerfils;
+        this.context = mContext;
+        this.clickListener = listener;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public PerfilAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                       int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.cell_perfil, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.bind(perfils.get(position),clickListener);
+        holder.perfilName.setText(perfils.get(position).getName());
+        Picasso.with(context).load(perfils.get(position).getFacebookImage()).into(holder.perfilPicture);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return perfils.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(Perfil item);
+    }
+
+}
--- app/src/main/java/com/opinnapp/opinnapp/tabholder/explore/tabs/PerfilsFragment.java
+++ app/src/main/java/com/opinnapp/opinnapp/tabholder/explore/tabs/PerfilsFragment.java
@@ -3,6 +3,8 @@ package com.opinnapp.opinnapp.tabholder.explore.tabs;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,7 +13,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.opinnapp.opinnapp.R;
+import com.opinnapp.opinnapp.tabholder.home.HomeFragment;
 import com.opinnapp.opinnapp.tabholder.home.tabs.PopularFragment;
+import com.opinnapp.opinnapp.tabholder.perfil.PerfilFragment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vzaffalon on 19/05/17.
@@ -22,6 +29,7 @@ public class PerfilsFragment extends Fragment {
     private RecyclerView recyclerView;
     private Context context;
     private View view;
+    private List<Perfil> perfils;
 
     // newInstance constructor for creating fragment with arguments
     public static PerfilsFragment newInstance() {
@@ -41,7 +49,32 @@ public class PerfilsFragment extends Fragment {
         view = inflater.inflate(R.layout.fragment_perfils, container, false);
         context = view.getContext();
         recyclerView = (RecyclerView) view.findViewById(R.id.fragment_perfils_recycler);
+        generatePerfils();
+        mountRecycler();
         return view;
+    }
+
+    private void mountRecycler() {
+        if (perfils != null) {
+            recyclerView.setHasFixedSize(true);
+            RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context);
+            recyclerView.setLayoutManager(layoutManager);
+            recyclerView.setAdapter(new PerfilAdapter(perfils, context,new PerfilAdapter.OnItemClickListener() {
+                @Override public void onItemClick(Perfil item) {
+                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_container, PerfilFragment.newInstance());
+                    transaction.commit();
+                }
+            }));
+        }
+    }
+
+    private void generatePerfils(){
+        perfils = new ArrayList<>();
+        perfils.add(new Perfil("Murilo","https://scontent-gru.xx.fbcdn.net/v/t1.0-9/602740_2839717690184_951506583_n.jpg?oh=9e9cdc3af65826e2db5a4384993a0f60&oe=599FA490"));
+        perfils.add(new Perfil("Cayke","https://scontent-gru.xx.fbcdn.net/v/t1.0-9/17308748_1279495765466119_5902661537556998459_n.jpg?oh=91cd66e6173a2312be22ac33d3ed4445&oe=59A3777D"));
+        perfils.add(new Perfil("Victor","https://scontent-gru.xx.fbcdn.net/v/t1.0-9/1907867_967496356601123_5817813594957135474_n.jpg?oh=cecab31754466c2419254d03b1933dac&oe=59A63C29"));
+
     }
 
 }
