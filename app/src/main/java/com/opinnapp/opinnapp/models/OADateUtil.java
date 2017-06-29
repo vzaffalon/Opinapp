5e54e1957c3b99ee77c6ffa21db5ab03f776b442
Cayke Prudente <cayke10@gmail.com>
2017-06-29 20:50:11 -0300

    - minor fix

--- app/src/main/java/com/opinnapp/opinnapp/models/OADateUtil.java
+++ app/src/main/java/com/opinnapp/opinnapp/models/OADateUtil.java
@@ -8,6 +8,41 @@ import java.util.Locale;
  */
 
 public class OADateUtil {
+    private static final int SECOND_MILLIS = 1000;
+    private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
+    private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
+    private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
+
+    public static String getTimeAgo(long time) {
+        if (time < 1000000000000L) {
+            // if timestamp given in seconds, convert to millis
+            time *= 1000;
+        }
+
+        long now = new Date().getTime();
+        if (time > now || time <= 0) {
+            return null;
+        }
+
+        final long diff = now - time;
+        if (diff < MINUTE_MILLIS) {
+            return "Agora mesmo.";
+        } else if (diff < 2 * MINUTE_MILLIS) {
+            return "Um minuto atrás.";
+        } else if (diff < 50 * MINUTE_MILLIS) {
+            return diff / MINUTE_MILLIS + " minutos atrás";
+        } else if (diff < 90 * MINUTE_MILLIS) {
+            return "Uma hora atrás";
+        } else if (diff < 24 * HOUR_MILLIS) {
+            return diff / HOUR_MILLIS + " horas atrás";
+        } else if (diff < 48 * HOUR_MILLIS) {
+            return "Ontem.";
+        } else {
+            return diff / DAY_MILLIS + " dias atrás.";
+        }
+    }
+
     public static String exparationString(Date endDate) {
         Date now = new Date();
 
@@ -16,9 +51,9 @@ public class OADateUtil {
         long minutes = seconds / 60;
         long hours = minutes / 60;
 
-        if (hours > 1)
-            return String.format(Locale.getDefault(), "%d:%d horas", hours, minutes%60);
+        if (hours > 0)
+            return String.format(Locale.getDefault(), "%02d:%02d horas", hours, minutes%60);
         else
-            return String.format(Locale.getDefault(), "%d:%d min", minutes, seconds);
+            return String.format(Locale.getDefault(), "%02d minutos", minutes);
     }
 }
